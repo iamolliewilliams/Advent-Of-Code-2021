dca388b0eec38061b2389fe265ad1f1cc97b64d9
iamolliewilliams <ollie.zzephos@gmail.com>
2021-12-03 10:48:39 +0000

    Day 3 solution - python

--- day-3/day-3.py
+++ day-3/day-3.py
@@ -8,8 +8,7 @@ with open("day-3/input.txt") as f:
     list_of_numbers = [line.strip() for line in f.readlines()]
 
 for position in range(0,len(list_of_numbers[0])):
-    one = 0
-    zero = 0
+    one,zero = 0,0
     for index in range(0,len(list_of_numbers)):
         if list_of_numbers[index][position] == '0':
             zero += 1
@@ -27,10 +26,8 @@ print('Part 1: {x}'.format(x=int(gamma,2)*int(epsilon,2)))
 list_of_numbers_copy = list_of_numbers.copy()
 position = 0
 while len(list_of_numbers) > 1:
-    one = 0
-    zero = 0
-    ones = []
-    zeros = []
+    one,zero = 0,0
+    ones,zeros = [],[]
     for index in range(0, len(list_of_numbers)):
         if list_of_numbers[index][position] == '0':
             zero += 1
@@ -50,10 +47,8 @@ oxygen = int(list_of_numbers[0], 2)
 list_of_numbers = list_of_numbers_copy
 position = 0
 while len(list_of_numbers) > 1:
-    one = 0
-    zero = 0
-    ones = []
-    zeros = []
+    one,zero = 0,0
+    ones,zeros = [],[]
     for index in range(0, len(list_of_numbers)):
         if list_of_numbers[index][position] == '0':
             zero += 1
